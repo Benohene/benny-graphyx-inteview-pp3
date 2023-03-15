d870f55dc666888187cd71a117d6e18a8aabf480
Benjamin Ohene Konadu <benohenekonadu@yahoo.com>
2023-03-15 05:27:03 +0000

    add the begin interview function with name entry and instructions

--- run.py
+++ run.py
@@ -14,12 +14,52 @@ SHEET = GSPREAD_CLIENT.open('benny_grapyhx-2023')
 
 #the beginning of the main terminal build
 
-def begin_interview():
+FULLNAME = ""
 
-def interview_main():
+def begin_interview():
+    """
+    Interview starts with a welcome note from the company,
+    moves to candidate to enter full name and get access to instructions,
+    then a good will message for the interview
+    """
+    print("WELCOME TO BENNY GRAPHYX INTERVIEW\n")
+
+    global FULLNAME
+    FULLNAME = input("Hello Candidate, Enter your Full Name: \n")
+
+    # Relaunches start quiz if no name is entered and user only clicks Enter
+    if FULLNAME == "":
+        print("Name is Invalid, Please Enter your Full Name!")
+        begin_interview()
+    else:
+        print(f"\nWelcome to the Job Interview Assessment, {FULLNAME}.\n")
+        
+        print("!!! PLEASE READ CAREFULLY THE INSTRUCTIONS.\n")
+        
+        print("-------------------------------------------------------.\n")
+        print("THERE ARE 10 INTERVIEW QUESTION, THIS WILL HELP US ASSESS YOU WELL.\n")
+        
+        print("EACH QUESTION COMES WILL 4 MULTI CHIOCE ANSWERS.\n")
+        
+        print("ENTER THE ANSWER FOR OPTIONS A,B,C AND D AND HIT ENTER.\n")
+        
+
+        start_interview = input("ARE YOU READY FOR THE INTERVIEW? (Y)YES or (N)NO: ")
+        if start_interview.lower() != "y":
+            quit()
+
+        print("LETS GET ON - GOOD LUCK :)")
+        
+
+# ------------------------
+
+"""def interview_main():
 
 def check_answer():
 
-def display
+def display'''
 
 #create dictionary of questions awith answers and options to choose from
+"""
+
+begin_interview()

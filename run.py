17f506f40e1dab8137793235c010508630ad1ee9
Benjamin Ohene Konadu <benohenekonadu@yahoo.com>
2023-03-15 05:12:22 +0000

    Add skeletal functions to run the app

--- run.py
+++ run.py
@@ -12,8 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('benny_grapyhx-2023')
 
-marks = SHEET.worksheet('overall_marks')
+#the beginning of the main terminal build
 
-data = marks.get_all_values()
+def begin_interview():
 
-print(data)
+def interview_main():
+
+def check_answer():
+
+def display
+
+#create dictionary of questions awith answers and options to choose from
